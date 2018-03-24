8ab3b68166e1b61644eba8e1bfc6980a7ca98e10
Nicolas Nytko <nicknytko@gmail.com>
2018-03-23 23:31:40 -0500

    add bootstrap

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const express = require("express"),
+var express = require("express"),
       path = require("path"),
       config = require("config"),
       family_db = require("./family-db.js");
-const app = express(),
+var app = express(),
       router = express.Router();
+
+function modulePath(path) {
+    return express.static(__dirname + "/node_modules/" + path);
+}
 
 family_db.cacheJsonTree();
 
-router.use("/d3", express.static(path.join(__dirname, "/node_modules/d3/build/")));
-router.use("/d3-dtree", express.static(path.join(__dirname, "/node_modules/d3-dtree/dist/")));
-router.use("/lodash", express.static(path.join(__dirname, "/node_modules/lodash/")));
+router.use("/d3", modulePath("/d3/build/"));
+router.use("/d3-dtree", modulePath("/d3-dtree/dist/"));
+router.use("/lodash", modulePath("/lodash/"));
+router.use("/bootstrap", modulePath("/bootstrap/dist/"));
+router.use("/jquery", modulePath("/jquery/dist/"));
+router.use("/popper", modulePath("/popper.js/dist/"));
 router.use(express.static("public"));
+
 router.get("/api/all", (req, res) => {
     res.json(family_db.getJsonTree());
 });
